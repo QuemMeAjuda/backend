6e2f34321c5799328a8613f23d12582f0b260b0a
wesley.anibal <wesley.anibal@ccc.ufcg.edu.br>
2018-05-10 22:13:20 -0300

    #Task01 - alterações na adição de ajuda,caso o aluno não exista

--- controllers/ajuda.controller.js
+++ controllers/ajuda.controller.js
@@ -1,19 +1,28 @@
 //Author: Wesley Gonçalves Anibal
 var Ajuda = require('../models/ajuda.model');
-var AlunoAjuda = require('../models/AlunoAJuda')
+var AlunoAjuda = require('../models/AlunoAJuda');
+var User = require('../models/user.model');
 
 exports.postAjuda = async function (req, res) {
     let ajuda = new Ajuda(req.body.ajuda);
-    ajuda.save((err, us)=>{
-        if(err){//caso dê erro
-            return res.status(400).json({message:"Falha na operacao", status:400});//retornando uma msg de erro
-        }else{//caso dê certo
-            return res.status(201).json({message:"Ajuda cadastrada com sucesso", status:201, data: ajuda});
+    let user = User.findById(req.body.alunoID,(err, us)=>{
+        if(err){
+            res.status(400).json({message:"Usuário não encontrado", status:400});
+        }else{
+            ajuda.save((err, us)=>{
+                if(err){//caso dê erro
+                    return res.status(400).json({message:"Falha na operacao", status:400});//retornando uma msg de erro
+                }else{//caso dê certo
+                    return res.status(201).json({message:"Ajuda cadastrada com sucesso", status:201, data: ajuda});
+                }
+            })
+            let alunoAjuda = new AlunoAjuda({alunoID: us._id, ajudaID: ajuda._id});
+            alunoAjuda.save((err, us)=>{});
         }
     })
 
-    let alunoAjuda = new AlunoAjuda({alunoID: req.body.alunoID, ajudaID: req.body.ajudaID});
-    alunoAjuda.save((err, us)=>{});
+
+
 };
 
 
--- models/AlunoAJuda.js
+++ models/AlunoAJuda.js
@@ -2,11 +2,6 @@ var mongoose = require('mongoose');
 
 var alunoAjudaSchema =  new mongoose.Schema({
 
-    id:{
-        type: String,
-        default: ''
-    },
-
     ajudaID: {
         type: String,
         default: ''
