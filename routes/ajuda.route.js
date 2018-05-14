e937651ff8d862aa3eccee3e606479076cf46d6d
ariannmichael <ariann.farias@ccc.ufcg.edu.br>
2018-05-14 20:53:26 -0300

    #Task 01 - rotas para modificar e deletar, ajudas e alunos

--- routes/ajuda.route.js
+++ routes/ajuda.route.js
@@ -6,5 +6,7 @@ const ajudaController = require('../controllers/ajuda.controller');
 router.post('/postAjuda', ajudaController.postAjuda);
 router.get('/getAjuda/:id' , ajudaController.getAjuda);
 router.get('/getAjudas', ajudaController.getAjudas);
+router.put('/updateAjuda', ajudaController.updateAjuda);
+router.delete('/deleteAjuda', ajudaController.deleteAjuda);
 
 module.exports = router;
