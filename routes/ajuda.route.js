e9b9a7649e528dc9e2c19ba5faf139c74b1235cd
niltonmng <nilton.ginani@ccc.ufcg.edu.br>
2018-06-06 20:24:18 -0300

    us08/task03 delete coment iniciado

--- routes/ajuda.route.js
+++ routes/ajuda.route.js
@@ -11,5 +11,6 @@ router.delete('/deleteAjuda', ajudaController.deleteAjuda);
 
 
 router.put('/putComentAjuda', ajudaController.putComentAjuda);
+router.delete('/deleteComentAjuda', ajudaController.deleteComentAjuda);
 
 module.exports = router;
